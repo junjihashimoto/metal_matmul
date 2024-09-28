7c4dd0772f36d21fcfe8943399eab3c7e2b9282c
Junji Hashimoto <junji.hashimoto@gree.net>
2024-09-28 17:54:04 +0900

    Initial Commit

---
+++ matmul/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  matmul
+//
+//  Created by Junji Hashimoto on 2024/09/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
